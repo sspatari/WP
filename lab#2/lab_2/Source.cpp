17ed95cda88970da9df17205bf7796e792ad7848
sspatari <spatari.stanislav@gmail.com>
2017-04-11 20:28:28 +0300

    add WM_SIZE for all elements

--- lab#2/lab_2/Source.cpp
+++ lab#2/lab_2/Source.cpp
@@ -23,6 +23,7 @@ button[] =
 
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK AboutDlgProc(HWND, UINT, WPARAM, LPARAM);
 void FillListBox(HWND hwndList);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
@@ -99,6 +100,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 	static int cxChar, cyChar;
 	RECT rect;
 	int i;
+	static char helloMessage[256] = "Good joke";
 
 
 
@@ -131,13 +133,65 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 			NULL);
 		hwndText = CreateWindow(TEXT("static"), NULL,
 			WS_CHILD | WS_VISIBLE | SS_LEFT,
-			cxChar, cyChar,
-			GetSystemMetrics(SM_CXSCREEN), cyChar,
+			(rect.right - strlen(helloMessage) * cxChar) / 2,
+			cyChar,
+			strlen(helloMessage) * cxChar,
+			cyChar,
 			hwnd, (HMENU)ID_TEXT,
 			(HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE),
 			NULL);
+
+		SetWindowText(hwndText, helloMessage);
 		FillListBox(hwndList);
 		return 0;
+	
+	case WM_SIZE:
+		GetClientRect(hwnd, &rect);
+		MoveWindow(hwndList,
+			(rect.right - 20 * cxChar) / 2,
+			rect.bottom / 2 - cyChar * 11,
+			cxChar * 18 + GetSystemMetrics(SM_CXVSCROLL),
+			cyChar * 10,
+			TRUE);
+		MoveWindow(hwndText,
+			(rect.right - strlen(helloMessage) * cxChar) / 2,
+			rect.bottom / 2 - cyChar * 15,
+			strlen(helloMessage) * cxChar,
+			cyChar,
+			TRUE);
+
+		for (i = 0; i < 3; i++) {
+			GetClientRect(hwnd, &rect);
+			MoveWindow(hwndButton[i],
+				(rect.right - 30 * cxChar) / 2,
+				(rect.bottom / 2) + cyChar * (1 + 3 * i),
+				30 * cxChar,
+				7 * cyChar / 4,
+				TRUE);
+		}
+		return 0;
+
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{ 
+		case 1:
+			DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), hwnd, AboutDlgProc); 
+			break;
+
+		case ID_MENU_ABOUT:
+			MessageBox(hwnd, TEXT("About Menu\n")
+				TEXT("\nFeel the pain"), TEXT("LAB2"), MB_OK | MB_ICONINFORMATION);
+			break;
+		case ID_MENU_HELP:
+			MessageBox(hwnd, TEXT("Help Menu\n")
+				TEXT("\nHelp cannot be provided :D"), TEXT("LAB2"), MB_OK | MB_ICONEXCLAMATION);
+			break;
+		case ID_MENU_DELETEMENU:
+			SetMenu(hwnd, NULL);
+			break;
+		}
+		return 0;
+
 	case WM_SYSCOMMAND:   //system menu 
 		switch (LOWORD(wParam)) {
 		case IDM_SYS_ABOUT:              
@@ -169,4 +223,22 @@ void FillListBox(HWND hwndList)
 		sprintf_s(str, "List Element #%d", i + 1);
 		SendMessage(hwndList, LB_INSERTSTRING, i, (LPARAM)str);
 	}
+}
+
+BOOL CALLBACK AboutDlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{ 
+	switch (message) 
+	{ case WM_INITDIALOG: 
+		return TRUE; 
+	case WM_COMMAND: 
+		switch (LOWORD(wParam)) 
+		{
+		case IDOK:
+		case IDCANCEL: 
+			EndDialog(hDlg, 0); 
+			return TRUE; 
+		} 
+		break;
+	}
+	return FALSE; 
 }
