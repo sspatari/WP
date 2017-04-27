f602508ceb6a72cbca594b63379aa217b6b3992c
sspatari <spatari.stanislav@gmail.com>
2017-04-28 01:14:40 +0300

    create all the needed variables and the WM_CREATE, added yoda.bmp to the project

--- lab#3/lab3/Source.cpp
+++ lab#3/lab3/Source.cpp
@@ -1,5 +1,5 @@
 #include <windows.h>
-
+#include <time.h>
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
@@ -47,8 +47,61 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+	static int cxClient, cyClient;
+	HDC hdc;
+	PAINTSTRUCT ps;
+	int i;
+	static RECT rect;
+	static HPEN pens[5];
+	int color_r, color_g, color_b;
+	static POINT bezier1[4], bezier2[4];
+	static POINT polygon[3];
+	static POINT circleCenter, squareCenter;
+	static RECT circleRect, square;
+	static int rePaint = 0;
+	static HDC memoryHDC;
+	static HBITMAP bitmapMem;
+	static HINSTANCE hInstance;
+	BITMAP bitmap;
+	static TCHAR quote[] = TEXT("Do. Or do not. There is no try.");
+	SIZE  lp_Size;
+	static HBRUSH selectbrush = NULL;
+	static HPEN circlePen;
+	static HBRUSH squareBrush;
+	static int changeCirclePen = 0, changeSquareBrush = 0, drawBezier = 0, delta = 0, erase = 0;
+	LOGPEN logpen;
+
+	srand(time(NULL));
+
 	switch (message)
 	{
+	case WM_CREATE:
+		GetClientRect(hwnd, &rect);
+		
+		// generating random colors for the figures
+		for (i = 0; i < 5; i++)
+		{
+			color_r = rand() % 256;
+			color_g = rand() % 256;
+			color_b = rand() % 256;
+			pens[i] = CreatePen(PS_SOLID, (i + 1) * 5 + (delta / 5), RGB(color_r, color_g, color_b));
+		}
+		hInstance = ((LPCREATESTRUCT)lParam)->hInstance;
+		hdc = GetDC(hwnd);
+		memoryHDC = CreateCompatibleDC(hdc);
+		bitmapMem = LoadBitmap(hInstance, MAKEINTRESOURCE(103));
+		SelectObject(memoryHDC, bitmapMem);
+		GetObject(bitmapMem, sizeof(bitmap), &bitmap);
+		GetTextExtentPoint32(memoryHDC, quote, strlen(quote), &lp_Size);
+		SetTextColor(memoryHDC, RGB(255, 255, 255));
+		SetBkColor(memoryHDC, RGB(0, 0, 0));
+		TextOut(memoryHDC, (bitmap.bmWidth - lp_Size.cx) / 2, (bitmap.bmHeight - lp_Size.cy) / 2, quote, strlen(quote));
+		SetScrollRange(hwnd, SB_VERT, 0, 10, TRUE);
+		SetScrollPos(hwnd, SB_VERT, 0, TRUE);
+		ReleaseDC(hwnd, hdc);
+		circlePen = CreatePen(PS_SOLID, 1, RGB(0, 0, 0));
+		squareBrush = (HBRUSH)GetStockObject(NULL_BRUSH);
+		return 0;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
