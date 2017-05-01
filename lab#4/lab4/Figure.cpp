00ee96b2195cfdf912530fa619855662cfa7c5cd
sspatari <spatari.stanislav@gmail.com>
2017-05-01 20:33:18 +0300

    add needed methods and create empty definition of them for the  Figure Class

---
+++ lab#4/lab4/Figure.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Figure.h"
+
+
+Figure::Figure(int radius, int x, int y)
+{
+}
+
+
+Figure::~Figure()
+{
+}
--- lab#4/lab4/Figure.h
+++ lab#4/lab4/Figure.h
@@ -17,5 +17,24 @@ private:
 public:
 	Figure(int radius, int x, int y);
 	~Figure();
+	int getType();
+	int getX();
+	int getY();
+	void setPosition(int x, int y);
+	void setSquare();
+	void setCircle();
+	void setRandomColor();
+	void setRandomVelocity();
+	void setRandomRadius();
+	void paint(HDC hdc);
+	void update();
+	bool collides(Figure figure);
+	bool collidesVerticalLine(int x);
+	bool collidesHorizontalLine(int y);
+	void invertVelocityX();
+	void invertVelocityY();
+	void increaseVelocity();
+	void decreaseVelocity();
+	void flipFigure();
 };
 
